d49ccd9427679b57658db7195e31c6b822ff5227
sachin <sachinkshuklaoo7@gamil.com>
2020-04-18 20:06:52 +0530

    CRUD Basics Done

--- app.js
+++ app.js
@@ -1,16 +1,92 @@
+const fs = require('fs');
 const express = require('express');
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.status(200).json({message: 'Hello from the server side', app: "Natours"});
+// app.get('/', (req, res) => {
+//   res
+//     .status(200)
+//     .json({ message: 'Hello from the server side', app: 'Natours' });
+// });
+
+// app.post('/', (req, res) => {
+//   res.send('You can send to this endpoint...');
+// });
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+// Get all tours
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: {
+      tours,
+    },
+  });
 });
 
-app.post('/', (req, res) => {
-    res.json({message: 'POST Req', app: "Natours"});
+// Get tour by ID
+app.get('/api/v1/tours/:id', (req, res) => {
+  const id = req.params.id * 1;
+
+  if (id > tours.length) {
+    return res.status(404).json({
+      status: 'failed',
+    });
+  }
+
+  const tour = tours[id];
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+// Create a new tour
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+
+  const newId = tours[tours.length - 1].id + 1;
+  const newTour = Object.assign({ id: newId }, req.body);
+
+  tours.push(newTour);
+
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      res.status(201).json({
+        status: 'success',
+        data: {
+          tour: newTour,
+        },
+      });
+    }
+  );
+});
+
+// Path tour data
+app.patch('/api/v1/tours/:id', (req, res) => {
+  res.status(200).json({
+    status: 'Done',
+  });
+});
+
+// Delete tour data
+app.delete('/api/v1/tours/:id', (req, res) => {
+  res.status(204).json({
+    status: 'Deleted',
+  });
 });
 
 const port = 3000;
 app.listen(port, () => {
-    console.log(`App running on port ${port}`);
-})
+  console.log(`App running on port ${port}`);
+});
